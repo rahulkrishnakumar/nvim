86e5615bf39a997536a181de72319dfa4f83c72b
Rahul Krishna <rahul2381994@gmail.com>
2024-07-05 19:47:15 +0000

    Emmet-ls auto install feature removed, due to annoying emmit suggessions on every no html writing.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,7 +12,8 @@ return {
         "tailwindcss-language-server",
         "typescript-language-server",
         "css-lsp",
-        "emmet-ls",
+        "emmet-language-server",
+        -- "emmet-ls",
       })
     end,
   },
@@ -49,15 +50,51 @@ return {
       lspconfig.lua_ls.setup({
         capabilities = capabilities,
       })
-      lspconfig.emmet_ls.setup({
-        capabilities = capabilities,
+      -- lspconfig.emmet_ls.setup({
+      --   capabilities = capabilities,
+      --   filetypes = {
+      --     "css",
+      --     "html",
+      --     "typescriptreact",
+      --   },
+      -- })
+      -- help and hover
+      lspconfig.emmet_language_server.setup({
         filetypes = {
           "css",
+          "eruby",
           "html",
+          "javascript",
+          "javascriptreact",
+          "less",
+          "sass",
+          "scss",
+          "pug",
           "typescriptreact",
         },
+        -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
+        -- **Note:** only the options listed in the table are supported.
+        init_options = {
+          ---@type table<string, string>
+          includeLanguages = {},
+          --- @type string[]
+          excludeLanguages = {},
+          --- @type string[]
+          extensionsPath = {},
+          --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+          preferences = {},
+          --- @type boolean Defaults to `true`
+          showAbbreviationSuggestions = true,
+          --- @type "always" | "never" Defaults to `"always"`
+          showExpandedAbbreviation = "always",
+          --- @type boolean Defaults to `false`
+          showSuggestionsAsSnippets = false,
+          --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+          syntaxProfiles = {},
+          --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+          variables = {},
+        },
       })
-      -- help and hover
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {
         desc = "Hover",
       })
