5955739adb668af9a07a81047e3f3e288cfc7ce4
Rahul Krishnakumar <rahul2381994@gmail.com>
2024-09-21 00:32:42 +0300

    Fix package-info color issue
    - fixed package-info coler issue - closes #7
    - ref discussiong: [ No Colors for both up_to_date and
    outdated](https://github.com/vuki656/package-info.nvim/issues/155)

--- lua/plugins/package-info.lua
+++ lua/plugins/package-info.lua
@@ -3,15 +3,19 @@ return {
     "vuki656/package-info.nvim",
     ft = "json",
     dependencies = { "MunifTanjim/nui.nvim" },
-    config = function()
-      require("package-info").setup({
-        autostart = false,
-        package_manager = "npm",
-        colors = {
-          outdated = "#db4b4b",
-        },
-        hide_up_to_date = true,
-      })
+    opts = {
+      autostart = false,
+      package_manager = "npm",
+      colors = {
+        up_to_date = "#0DB9D7",
+        outdated = "#db4b4b",
+      },
+      hide_up_to_date = true,
+    },
+    config = function(_, opts)
+      require("package-info").setup(opts)
+      vim.cmd([[highlight PackageInfoUpToDateVersion guifg=]] .. opts.colors.up_to_date)
+      vim.cmd([[highlight PackageInfoOutdatedVersion guifg=]] .. opts.colors.outdated)
     end,
   },
 }
