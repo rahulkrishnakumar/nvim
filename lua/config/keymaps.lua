08849995b7f4afe0ac2150e1229a5a809e8f24ce
Rahul Krishnakumar <rahul2381994@gmail.com>
2024-09-20 23:52:45 +0300

    Add new plugin - package-info

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,8 @@
 -- Add any additional keymaps here
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
+local set_keymap = vim.api.nvim_set_keymap
+
 -- Increment/decrement
 keymap.set("n", "+", "<C-a>")
 keymap.set("n", "-", "<C-x>")
@@ -44,4 +46,36 @@ end, opts)
 -- keymap for inc rename
 keymap.set("n", "<leader>rn", ":IncRename ")
 -- diagnostics (trouble plugin)
-
+--
+--
+-- package-info keymaps
+set_keymap(
+  "n",
+  "<leader>cpt",
+  "<cmd>lua require('package-info').toggle()<cr>",
+  { silent = true, noremap = true, desc = "Toggle" }
+)
+set_keymap(
+  "n",
+  "<leader>cpd",
+  "<cmd>lua require('package-info').delete()<cr>",
+  { silent = true, noremap = true, desc = "Delete package" }
+)
+set_keymap(
+  "n",
+  "<leader>cpu",
+  "<cmd>lua require('package-info').update()<cr>",
+  { silent = true, noremap = true, desc = "Update package" }
+)
+set_keymap(
+  "n",
+  "<leader>cpi",
+  "<cmd>lua require('package-info').install()<cr>",
+  { silent = true, noremap = true, desc = "Install package" }
+)
+set_keymap(
+  "n",
+  "<leader>cpc",
+  "<cmd>lua require('package-info').change_version()<cr>",
+  { silent = true, noremap = true, desc = "Change package version" }
+)
